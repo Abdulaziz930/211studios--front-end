56b9935b10b15ccb1d2e91854f93889133f84185
Abdulaziz930 <abdulazizak@code.edu.az>
2021-10-14 01:03:07 +0400

    initial folder structure done

---
+++ src/hooks/useAsyncData.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState, useCallback } from "react";
+import { IAsyncData } from "../models/models";
+import { INITIAL_ASYNC_DATA } from "../utils/consts";
+
+export function useAsyncData<T>(url: string): [IAsyncData<T>, () => void] {
+  const [data, setData] = useState<IAsyncData<T>>(INITIAL_ASYNC_DATA);
+
+  const getData = useCallback(() => {
+    if (!!url) {
+      axios
+        .get<T>(url)
+        .then(({ data }) => {
+          setData((oldData) => ({ ...oldData, data, error: undefined }));
+        })
+        .catch((error) => {
+          setData({ data: undefined, error: error.toString() });
+        });
+    }
+  }, [url]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return [data, getData];
+}
