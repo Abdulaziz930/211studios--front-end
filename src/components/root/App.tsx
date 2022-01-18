61db3b21ba4fd8bc4821646b9520ff8c89500dff
Abdulaziz930 <abdulazizak@code.edu.az>
2022-01-19 01:52:19 +0400

    spinner and error page is added

--- src/components/root/App.tsx
+++ src/components/root/App.tsx
@@ -1,13 +1,25 @@
 import Navi from "../layout/header/Navi";
 import Routes from "../routes/Routes";
 import Footer from "../layout/footer/Footer";
+import { Offline, Online } from "react-detect-offline";
+import Error from "../pages/error/Error";
 
 function App() {
   return (
     <>
-      <Navi />
-      <Routes />
-      <Footer />
+      <Offline>
+        <Error
+          statusCode={503}
+          title='Connection lost'
+          description='Looks like your internet has lost, please check your internet connection and try connecting to the internet again'
+          buttonIsExist={false}
+        />
+      </Offline>
+      <Online>
+        <Navi />
+        <Routes />
+        <Footer />
+      </Online>
     </>
   );
 }
